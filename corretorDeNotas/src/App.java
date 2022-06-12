0b101f0e52307a57bab6f38c04fdaa26bf87ac96
Rhavy <81573087+rhavyyz@users.noreply.github.com>
2022-06-11 23:58:17 -0300

    Delete corretorDeNotas directory

--- corretorDeNotas/src/App.java
+++
@@ -1,8 +0,0 @@
-import disciplina.Disciplina;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Disciplina d = new Disciplina(1, "POO", "/disciplinas/gab.txt");
-        d.inserirNotas(2);
-    }
-}
